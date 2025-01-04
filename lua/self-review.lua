26b6469340f57415966eb881c55a33f681a4c81a
Rafael Rahal <rrrahal@gmail.com>
2025-01-04 17:13:18 +0000

    feat: add header window with file path

--- lua/self-review.lua
+++ lua/self-review.lua
@@ -1,45 +1,49 @@
-local M = {}
 local status = require("git.status")
-local windows = require("window")
+local windows = require("windows")
 local diffMod = require("git.diff")
 
 -- TODO: add meta information about the file (filename) and the amount of diffs
 -- TODO: add footer with keymaps/actions
 -- TODO: add keymap to add diff
 
-M.setup = function() end
+local M = {}
+
+local set_window_content = function(f_windows, header, body, footer)
+  vim.api.nvim_buf_set_lines(f_windows.body.buf, 0, -1, false, body)
+  vim.api.nvim_buf_set_lines(f_windows.header.buf, 0, -1, false, header)
+end
 
 -- TODO: it breaks when there are no diffs
 -- TODO: is it safe to add those keymaps? double check
 M.start_diff = function()
   local files = status.changed_files()
-  local w = windows.create_window()
-  vim.bo[w.buf].filetype = "diff"
+  local floating_windows = windows.create_windows()
+  vim.bo[floating_windows.body.buf].filetype = "diff"
 
   local current_diff = 1
   local diff = diffMod.get_diff(files[current_diff].path)
-  vim.api.nvim_buf_set_lines(w.buf, 0, -1, false, diff)
+  set_window_content(floating_windows, { files[current_diff].path }, diff)
 
   vim.keymap.set("n", "n", function()
     current_diff = math.min(current_diff + 1, #files)
     local newDiff = diffMod.get_diff(files[current_diff].path)
-    vim.api.nvim_buf_set_lines(w.buf, 0, -1, false, newDiff)
+    set_window_content(floating_windows, { files[current_diff].path }, newDiff)
   end, {
-    buffer = w.buf,
+    buffer = floating_windows.body.buf,
   })
 
   vim.keymap.set("n", "p", function()
     current_diff = math.max(current_diff - 1, 1)
     local newDiff = diffMod.get_diff(files[current_diff].path)
-    vim.api.nvim_buf_set_lines(w.buf, 0, -1, false, newDiff)
+    set_window_content(floating_windows, { files[current_diff].path }, newDiff)
   end, {
-    buffer = w.buf,
+    buffer = floating_windows.body.buf,
   })
 
   vim.keymap.set("n", "q", function()
-    vim.api.nvim_win_close(w.win, true)
+    vim.api.nvim_win_close(floating_windows.body.win, true)
   end, {
-    buffer = w.buf,
+    buffer = floating_windows.body.buf,
   })
 end
 
---
+++ lua/windows.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+local windows_configs = function()
+  local width = vim.o.columns
+  local height = vim.o.lines
+
+  local win_width = math.floor(width * 0.8)
+  local win_height = math.floor(height * 0.8)
+
+  local row = math.floor((height - win_height) / 2)
+  local col = math.floor((width - win_width) / 2)
+
+  return {
+    header = {
+      relative = "editor",
+      width = win_width,
+      height = 1,
+      row = row,
+      col = col,
+      style = "minimal",
+    },
+
+    body = {
+      relative = "editor",
+      width = win_width,
+      height = win_height - 1,
+      row = row + 1,
+      col = col,
+      style = "minimal",
+    },
+  }
+end
+
+local create_window = function(config)
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+
+  local win = vim.api.nvim_open_win(buf, true, config)
+  vim.api.nvim_win_set_option(win, "cursorline", true)
+
+  return { win = win, buf = buf }
+end
+
+M.create_windows = function()
+  local cfgs = windows_configs()
+
+  local header = create_window(cfgs.header)
+  local body = create_window(cfgs.body)
+
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = body.buf,
+    callback = function()
+      pcall(vim.api.nvim_win_close, header.win, true)
+    end,
+  })
+
+  return { header = header, body = body }
+end
+
+return M
