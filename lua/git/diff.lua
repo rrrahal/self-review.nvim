e9141871b82fa6a210f61a3a131db49f3188e34f
Rafael Rahal <rrrahal@gmail.com>
2025-01-08 10:27:00 +0000

    feat: remove header from diff

--- lua/git/diff.lua
+++ lua/git/diff.lua
@@ -1,8 +1,19 @@
 local M = {}
 
+local parse_diff = function(diffArray)
+  -- removes the header
+  table.remove(diffArray, 1)
+  table.remove(diffArray, 1)
+  table.remove(diffArray, 1)
+  table.remove(diffArray, 1)
+  table.remove(diffArray, 1)
+  return diffArray
+end
+
 M.get_diff = function(filename)
   local diff = vim.fn.system("git diff " .. filename)
-  return vim.split(diff, "\n")
+  local diffArray = vim.split(diff, "\n")
+  return parse_diff(diffArray)
 end
 
 return M
